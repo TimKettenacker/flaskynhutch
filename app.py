0117a3baa1e8ad33b0ed9f08c444a193463a138c
Tim <timkettenacker@Tims-MacBook-Pro.local>
2021-03-25 08:11:05 +0100

    added an api

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 from flask import render_template
-from flask import request
+from flask import request, jsonify
 import csv
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def root():
@@ -16,6 +15,15 @@ def root():
 
         return render_template('home.html', data=data, fieldnames=fieldnames, len=len)
 
+@app.route('/api/v1/data', methods=['GET'])
+def api_all():
+    with open('example_data.csv', 'r') as f:
+        # init dataset
+        data = [dict(item) for item in csv.DictReader(f)]
+
+        fieldnames = [key for key in data[0].keys()]
+
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
